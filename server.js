85beba70d302c9ce4336b8dcfff335e777929dc0
sali <sali.taymor@gmail.com>
2022-06-29 20:26:18 +0200

    root -post flashcard

--- server.js
+++ server.js
@@ -1,12 +1,24 @@
 const express = require('express');
- 
+const { sequelize, Flashcard } = require("./models");
 const app = express();
-const port = 3022;
+const port = 3032;
+app.use (express.json())
  
 app.get('/', (req, res) => {
     res.send('<h1>Flashcard API</h1>');
 });
+app.post("/flashcards", async (req, res) => {
+    const { category, front, back } = req.body;
+    try {
+        const flashcard = await Flashcard.create({ category, front, back });
+        return res.json(flashcard);
+    } catch (err) {
+        console.log(err);
+        return res.status(500);
+    }
+});
  
-app.listen(port, () => {
+app.listen(port, async () => {
     console.log(`Listening on http://localhost:${port}`);
+    await sequelize.sync();
 });
